c5563d608b16f9d0d05ebb1689eb9ac7daa71182
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2021-05-17 15:41:56 +0200

    Credit calculation rules added in simulation model

--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -54,4 +54,29 @@ class Simulation < ApplicationRecord
       'and' => 'et'
     }
   end
+
+  # Credit calculation methods
+
+  def credit_reimbursement_per_year
+    credit_amount / credit_duration
+  end
+
+  def credit_reimbursement_per_month
+    credit_amount / (credit_duration * 12)
+  end
+
+  def remaining_credit_amount(credit_ongoing_year)
+    credit_amount - (credit_ongoing_year * credit_reimbursement_per_year)
+  end
+
+  def credit_cost_for_year(credit_ongoing_year)
+    credit_rate * remaining_credit_amount(credit_ongoing_year) + credit_insurance_rate * credit_amount
+  end
+
+  def total_credit_cost
+    credit_costs_array = (1..credit_duration).each_with_object([]) do |credit_ongoing_year, array|
+      array << credit_cost_for_year(credit_ongoing_year)
+    end
+    credit_costs_array.sum
+  end
 end
