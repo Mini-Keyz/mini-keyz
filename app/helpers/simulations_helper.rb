1f51eaa89f7436659c4d476c7f1276ae5f07a499
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2021-05-25 19:32:00 +0200

    Tooltip gets the correspinding value of sim instance

--- app/helpers/simulations_helper.rb
+++ app/helpers/simulations_helper.rb
@@ -1,4 +1,11 @@
 module SimulationsHelper
+  FORMULA_ELEMENT_TO_CORRESPONDING_SIMULATION_VARIABLE = {
+    "test": 'hey oh',
+    "Loyer annuel HC": 'house_rent_amount_per_year',
+    "Prix d'achat": 'house_price_bought_amount',
+    "Frais de notaire": 'xxx'
+  }.freeze
+
   def errors_for(model, attribute)
     if model.errors[attribute].present?
       content_tag :span, class: 'error_explanation' do
@@ -7,19 +14,25 @@ module SimulationsHelper
     end
   end
 
-  def tooltip_formula(formula_array)
-    formula_array.map!.with_index { |element, i| i.even? ? tooltip(element) : element }.join(' ')
+  def tooltip_formula(formula_elements_array)
+    formula_elements_array.map!.with_index do |formula_element, i|
+      i.even? ? tooltip(formula_element) : formula_element
+    end.join(' ')
   end
 
-  def tooltip(element)
+  def tooltip(formula_element)
     tooltip = 'relative inline-block border-b border-dotted border-gray-400 group'
     tooltiptext = 'absolute z-10 bottom-full left-1/2 transform -translate-x-1/2 bg-gray-600 text-white text-center px-5 rounded-md invisible group-hover:visible'
 
     "<span class='#{tooltip}'>
-      #{element}
+      #{formula_element}
       <span class='#{tooltiptext}'>
-        Tooltip text
+        <%= simulation.#{convert_formula_element_to_corresponding_simulation_variable(formula_element)} %>
       </span>
     </span>"
+  end
+
+  def convert_formula_element_to_corresponding_simulation_variable(formula_element)
+    FORMULA_ELEMENT_TO_CORRESPONDING_SIMULATION_VARIABLE[formula_element.to_sym]
   end
 end
