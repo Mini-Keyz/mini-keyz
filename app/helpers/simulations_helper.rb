6c1ddab884e38d7d3dca76e3dc226c71cce8eddf
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2021-05-25 22:07:03 +0200

    Tooltip displays the corresponding value of sim instance variable

--- app/helpers/simulations_helper.rb
+++ app/helpers/simulations_helper.rb
@@ -22,24 +22,6 @@ module SimulationsHelper
     end
   end
 
-  def tooltip_formula(formula_elements_array)
-    formula_elements_array.map!.with_index do |formula_element, i|
-      i.even? ? tooltip(formula_element) : formula_element
-    end.join(' ')
-  end
-
-  def tooltip(formula_element)
-    tooltip = 'relative inline-block border-b border-dotted border-gray-400 group'
-    tooltiptext = 'absolute z-10 bottom-full left-1/2 transform -translate-x-1/2 bg-gray-600 text-white text-center px-5 rounded-md invisible group-hover:visible'
-
-    "<span class='#{tooltip}'>
-      #{formula_element}
-      <span class='#{tooltiptext}'>
-        <%= simulation.#{convert_formula_element_to_corresponding_simulation_variable(formula_element)} %>
-      </span>
-    </span>"
-  end
-
   def convert_formula_element_to_corresponding_simulation_variable(formula_element)
     FORMULA_ELEMENT_TO_CORRESPONDING_SIMULATION_VARIABLE[formula_element.to_sym]
   end
