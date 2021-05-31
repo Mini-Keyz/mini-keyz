8d4d206446b4a7bbd07283686dde7625fae1834b
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2021-05-31 14:51:06 +0200

    Modified gh workflow to include our brand new tests v7

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,13 +61,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  # Shoulda-Matchers init config
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      with.test_framework :rspec
-      with.library :rails
-    end
   end
 
   # Cleans the Ruby on Rails project tests database in between the test suite runs
