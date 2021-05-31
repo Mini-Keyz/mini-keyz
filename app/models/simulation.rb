86723199d6ed8ce2799b98e33a9345e9c686ed50
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2021-05-31 13:55:43 +0200

    Corrected validation for fiscal_nb_parts (numericality instead of only_integer) + corrected tests

--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -20,7 +20,7 @@ class Simulation < ApplicationRecord
   validates :fiscal_regimen, presence: true, inclusion: { in: proc { FISCAL_REGIMEN_AVAILABLE } }
   validates :fiscal_revenues_p1, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0  }
   validates :fiscal_revenues_p2, allow_blank: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :fiscal_nb_parts, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :fiscal_nb_parts, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
   include(CreditFormulas)
   include(IncomeTaxesFormulas)
