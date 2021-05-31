59e0bf1e6626d1274d573aee2c89e899c9c4e683
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2021-05-31 20:26:20 +0200

    Unsuccessfully tried to format numbers in input fields

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
 
-// Import Tailwind
+// Import dependencies
 import "stylesheets/application";
 
 // Import plugins
