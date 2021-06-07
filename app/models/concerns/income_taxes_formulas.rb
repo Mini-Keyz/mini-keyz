c3332904a69decfb636f37ef20f3f1984422c77a
Mathieu EUSTACHY <mathieu.eustachy@gmail.com>
2021-06-07 12:32:47 +0200

    Corrected fiscal_nb_parts calculation incurred from children

--- app/models/concerns/income_taxes_formulas.rb
+++ app/models/concerns/income_taxes_formulas.rb
@@ -73,27 +73,54 @@ module IncomeTaxesFormulas
   def calc_fiscal_nb_parts
     case fiscal_marital_status
     when 'Marié / Pacsé'
-      FISCAL_NB_PARTS_FOR_MARRIED_COUPLE + calc_fiscal_nb_parts_for_dependent_children + calc_fiscal_nb_parts_for_alternate_custody_children
+      FISCAL_NB_PARTS_FOR_MARRIED_COUPLE + calc_fiscal_nb_parts_incurred_from_children
     when 'Célibataire'
-      FISCAL_NB_PARTS_FOR_SINGLE_PERSON + calc_fiscal_nb_parts_for_dependent_children + calc_fiscal_nb_parts_for_alternate_custody_children
+      FISCAL_NB_PARTS_FOR_SINGLE_PERSON + calc_fiscal_nb_parts_incurred_from_children
     end
   end
 
-  def calc_fiscal_nb_parts_for_dependent_children
-    if fiscal_nb_dependent_children <= 2
-      fiscal_nb_dependent_children * 0.5
-    elsif fiscal_nb_dependent_children >= 3
-      1 + (fiscal_nb_dependent_children - 2)
+  def calc_fiscal_nb_parts_incurred_from_children
+    total_nb_children = fiscal_nb_dependent_children + fiscal_nb_alternate_custody_children
+
+    if total_nb_children <= 2
+      0.5 * fiscal_nb_dependent_children + 0.25 * fiscal_nb_alternate_custody_children
+
+    elsif total_nb_children >= 3
+
+      if fiscal_nb_dependent_children == 0
+        0.5 + (fiscal_nb_alternate_custody_children - 2) * 0.5
+
+      elsif fiscal_nb_dependent_children == 1
+        if fiscal_nb_alternate_custody_children == 2
+          1.25
+        elsif fiscal_nb_alternate_custody_children >= 3
+          1.25 + (fiscal_nb_alternate_custody_children - 2) * 0.5
+        end
+
+      elsif fiscal_nb_dependent_children == 2
+        1 + fiscal_nb_alternate_custody_children * 0.5
+
+      elsif fiscal_nb_dependent_children >= 3
+        2 + fiscal_nb_alternate_custody_children * 0.5
+      end
     end
   end
 
-  def calc_fiscal_nb_parts_for_alternate_custody_children
-    if fiscal_nb_alternate_custody_children <= 2
-      fiscal_nb_alternate_custody_children * 0.25
-    elsif fiscal_nb_alternate_custody_children >= 3
-      0.5 + (fiscal_nb_alternate_custody_children - 2) * 0.5
-    end
-  end
+  # def calc_fiscal_nb_parts_for_dependent_children
+  #   if fiscal_nb_dependent_children <= 2
+  #     fiscal_nb_dependent_children * 0.5
+  #   elsif fiscal_nb_dependent_children >= 3
+  #     1 + (fiscal_nb_dependent_children - 2)
+  #   end
+  # end
+
+  # def calc_fiscal_nb_parts_for_alternate_custody_children
+  #   if fiscal_nb_alternate_custody_children <= 2
+  #     fiscal_nb_alternate_custody_children * 0.25
+  #   elsif fiscal_nb_alternate_custody_children >= 3
+  #     0.5 + (fiscal_nb_alternate_custody_children - 2) * 0.5
+  #   end
+  # end
 
   def calc_family_quotient_amount(global_net_taxable_income_amount, fiscal_nb_parts)
     global_net_taxable_income_amount / fiscal_nb_parts
