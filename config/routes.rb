7c9c09a74bb324a76055aa52ef2458df25af821a
Mth0158 <mathieu.eustachy@gmail.com>
2021-07-16 09:39:11 +0200

    Testing subdomain, constraint 'www' for other routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,15 @@ Rails.application.routes.draw do
 
   constraints subdomain: 'api' do
     get 'about', to: 'pages#about'
-  end
-  get 'contact', to: 'pages#contact'
-
-  authenticated :user do
-    resources :simulations, only: %i[index show new create update destroy]
-    resources :users, only: [:show]
+    get 'contact', to: 'pages#contact'
   end
 
-  resources :simulations, only: %i[show new create]
+  constraints subdomain: 'www' do
+    authenticated :user do
+      resources :simulations, only: %i[index show new create update destroy]
+      resources :users, only: [:show]
+    end
+
+    resources :simulations, only: %i[show new create]
+  end
 end
