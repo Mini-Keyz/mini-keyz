bdd046526d8d40b1ad480dd552382f1b8d8c820b
Mth0158 <mathieu.eustachy@gmail.com>
2021-07-21 10:33:46 +0200

    Deleted user_mailer which was a test for using rails mailer system

--- spec/mailers/previews/user_preview.rb
+++
@@ -1,8 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/user
-class UserPreview < ActionMailer::Preview
-  # Preview this email at http://localhost:3000/rails/mailers/user/welcome
-  def welcome
-    user = User.first
-    UserMailer.with(user: user).welcome
-  end
-end
