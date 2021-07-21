2c3b7f92927a745ecc8be1baf3ab5038f9eb19fb
Mth0158 <mathieu.eustachy@gmail.com>
2021-07-21 10:39:13 +0200

    Overrided rails default mailer path

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
+  # Overriding Rails default mailer path by creating a mailers folder
+  default template_path: -> { "mailers/#{self.class.name.underscore}" }
+
   default from: 'no-reply@mini-keyz.com'
   layout 'mailer'
 end
--- app/mailers/simulation_mailer.rb
+++ app/mailers/simulation_mailer.rb
@@ -1,4 +1,5 @@
 class SimulationMailer < ApplicationMailer
+  # The email that will be sent to the user if he wants to have the simulation by email
   def send_it_to_me
     @user = params[:user]
     mail to: @user.email, subject: 'Bienvenue sur Mini-Keyz !'
