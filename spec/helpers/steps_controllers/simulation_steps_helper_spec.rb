403af8ab1600da544027bb22b00c6ac47a9c1b80
Mth0158 <mathieu.eustachy@gmail.com>
2021-08-26 14:03:18 +0200

    Generated steps_controllers folder and simulation_steps_controller

---
+++ spec/helpers/steps_controllers/simulation_steps_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the StepsControllers::SimulationStepsHelper. For example:
+#
+# describe StepsControllers::SimulationStepsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe StepsControllers::SimulationStepsHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
