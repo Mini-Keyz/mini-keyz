1d43118eca127aa1470eea6b03ee9ac980d0d48b
Mth0158 <mathieu.eustachy@gmail.com>
2021-09-24 22:17:28 +0200

    Rendering style is ok on checked, not ok on unchecked, need to take into account the case when rerendering page (ie back, reload, ...)

--- app/javascript/controllers/form_radio_buttons_controller.js
+++ app/javascript/controllers/form_radio_buttons_controller.js
@@ -8,95 +8,105 @@ export default class extends Controller {
   toggle(event) {
     const btnColor = this.radioParentTarget.id.split("-")[2];
     console.log(btnColor);
+
+    const blue = {
+      classToApplyNotChecked: [
+        "flex",
+        "justify-center",
+        "items-center",
+        "bg-white",
+        "rounded",
+        "border-2",
+        "border-blue-200",
+      ],
+      classToApplyChecked: [
+        "flex",
+        "justify-center",
+        "items-center",
+        "bg-blue-200",
+        "rounded",
+        "border-2",
+        "border-blue-200",
+        "outline-none",
+        "ring-1",
+        "ring-blue-400",
+        "border-transparent",
+      ],
+    };
+
+    const red = {
+      classToApplyNotChecked: [
+        "flex",
+        "justify-center",
+        "items-center",
+        "bg-white",
+        "rounded",
+        "border-2",
+        "border-red-200",
+      ],
+      classToApplyChecked: [
+        "flex",
+        "justify-center",
+        "items-center",
+        "bg-red-200",
+        "rounded",
+        "border-2",
+        "border-red-200",
+        "outline-none",
+        "ring-1",
+        "ring-red-400",
+        "border-transparent",
+      ],
+    };
+
+    const yellow = {
+      classToApplyNotChecked: [
+        "flex",
+        "justify-center",
+        "items-center",
+        "bg-white",
+        "rounded",
+        "border-2",
+        "border-yellow-200",
+      ],
+      classToApplyChecked: [
+        "flex",
+        "justify-center",
+        "items-center",
+        "bg-yellow-200",
+        "rounded",
+        "border-2",
+        "border-yellow-200",
+        "outline-none",
+        "ring-1",
+        "ring-yellow-400",
+        "border-transparent",
+      ],
+    };
+
     console.log(this.radioBtnTarget);
     console.log(this.radioValueTarget.checked);
     console.log(this.radioParentTarget.classList);
     // Check or uncheck on click
     event.preventDefault();
     this.radioValueTarget.checked = !this.radioValueTarget.checked;
+    console.log(this.radioValueTarget.checked);
 
     // Change radio button style depending on styling
     if (this.radioValueTarget.checked) {
-      this.radioParentTarget.classList.remove(btnColor.classToApplyNotChecked);
-      this.radioParentTarget.classList.add(btnColor.classToApplyChecked);
+      this.radioParentTarget.classList.remove(
+        ...eval(btnColor).classToApplyNotChecked
+      );
+      this.radioParentTarget.classList.add(
+        ...eval(btnColor).classToApplyChecked
+      );
     } else {
-      this.radioParentTarget.classList.add(btnColor.classToApplyNotChecked);
-      this.radioParentTarget.classList.remove(btnColor.classToApplyChecked);
+      this.radioParentTarget.classList.add(
+        ...eval(btnColor).classToApplyNotChecked
+      );
+      this.radioParentTarget.classList.remove(
+        ...eval(btnColor).classToApplyChecked
+      );
     }
   }
-
-  blue = {
-    classToApplyNotChecked: [
-      "flex",
-      "justify-center",
-      "items-center",
-      "bg-white",
-      "rounded",
-      "border-2",
-      "border-blue-200",
-    ],
-    classToApplyChecked: [
-      "flex",
-      "justify-center",
-      "items-center",
-      "bg-blue-200",
-      "rounded",
-      "border-2",
-      "border-blue-200",
-      "outline-none",
-      "ring-1",
-      "ring-blue-400",
-      "border-transparent",
-    ],
-  };
-
-  red = {
-    classToApplyNotChecked: [
-      "flex",
-      "justify-center",
-      "items-center",
-      "bg-white",
-      "rounded",
-      "border-2",
-      "border-red-200",
-    ],
-    classToApplyChecked: [
-      "flex",
-      "justify-center",
-      "items-center",
-      "bg-red-200",
-      "rounded",
-      "border-2",
-      "border-red-200",
-      "outline-none",
-      "ring-1",
-      "ring-red-400",
-      "border-transparent",
-    ],
-  };
-
-  yellow = {
-    classToApplyNotChecked: [
-      "flex",
-      "justify-center",
-      "items-center",
-      "bg-white",
-      "rounded",
-      "border-2",
-      "border-yellow-200",
-    ],
-    classToApplyChecked: [
-      "flex",
-      "justify-center",
-      "items-center",
-      "bg-yellow-200",
-      "rounded",
-      "border-2",
-      "border-yellow-200",
-      "outline-none",
-      "ring-1",
-      "ring-yellow-400",
-      "border-transparent",
-    ],
-  };
 }
