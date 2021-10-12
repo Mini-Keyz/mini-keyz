0c45114400eb2fd31b23b8c13b3e2bc433ac3e1a
Mth0158 <mathieu.eustachy@gmail.com>
2021-10-12 10:55:59 +0200

    Added locale logic in our application_controller, we are now able to extract the user's locale from the params or the http headers

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module MiniKeyz
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
     config.i18n.default_locale = :fr
+    config.i18n.available_locales = %i[en fr]
 
     # Config subdomain
     config.action_dispatch.tld_length = Integer(ENV['TLD_LENGTH'] || 1)
