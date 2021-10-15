6fc735de7d804e34b4e98d2b484ff36a714c4b05
Mth0158 <mathieu.eustachy@gmail.com>
2021-10-15 18:25:27 +0200

    Refactored step 1

--- app/javascript/controllers/input_controller.js
+++ app/javascript/controllers/input_controller.js
@@ -25,7 +25,7 @@ export default class extends Controller {
       });
       console.log(hasError);
 
-      const inputFields = inputWrapper.filter((element) => {
+      const inputParentField = inputWrapper.filter((element) => {
         // Has to loop again because of form_for generating <div class="form_with_errors">...</div> around our label and input fields
         console.log(element);
         const children = Array.from(element.children);
@@ -36,16 +36,16 @@ export default class extends Controller {
         console.log(inputChildrenFields);
         return inputChildrenFields.length;
       });
-      console.log(inputFields);
+      console.log(inputParentField);
 
-      if (inputFields.length === 0) return;
-      console.log(inputFields[0].children);
-      const isInputTypingField = inputFields[0].children.length === 1;
-      console.log(isInputTypingField);
+      if (inputParentField.length === 0) return;
+
+      const inputFields = inputParentField[0].children;
+      const isInputTypingField = inputFields.length === 1;
 
       if (hasError && isInputTypingField) {
-        inputFields[0].children[0].classList.remove(`input-${formColor}`);
-        inputFields[0].children[0].classList.add("input-error");
+        inputFields[0].classList.remove(`input-${formColor}`);
+        inputFields[0].classList.add("input-error");
       }
     });
   }
