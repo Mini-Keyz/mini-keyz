528c6792caf1978825ad152b27ede4c9739ea35a
Mth0158 <mathieu.eustachy@gmail.com>
2021-10-16 14:01:59 +0200

    Removed console.log statements

--- app/javascript/controllers/input_controller.js
+++ app/javascript/controllers/input_controller.js
@@ -17,26 +17,19 @@ export default class extends Controller {
     const formFields = Array.from(this.formWrapperTarget.children);
     formFields.forEach((formField) => {
       const inputWrapper = Array.from(formField.children);
-      console.log(inputWrapper);
 
       const hasError = inputWrapper.find((element) => {
-        console.log(element.classList.contains("error_explanation"));
         return element.classList.contains("error_explanation");
       });
-      console.log(hasError);
 
       const inputParentField = inputWrapper.filter((element) => {
         // Has to loop again because of form_for generating <div class="form_with_errors">...</div> around our label and input fields
-        console.log(element);
         const children = Array.from(element.children);
-        console.log(children);
         const inputChildrenFields = children.filter((child) => {
           return child.tagName === "INPUT";
         });
-        console.log(inputChildrenFields);
         return inputChildrenFields.length;
       });
-      console.log(inputParentField);
 
       if (inputParentField.length === 0) return;
 
