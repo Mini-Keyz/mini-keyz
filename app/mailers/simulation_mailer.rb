e59ebd20f89a437be9218079cdd52cf81389e325
Mth0158 <mathieu.eustachy@gmail.com>
2021-10-23 18:27:12 +0200

    Changed simulation_controller, simulation_mail, simulatio show and created associated mails for the case when a user is not logged in (unknown) and wants to get its simulation by email

--- app/mailers/simulation_mailer.rb
+++ app/mailers/simulation_mailer.rb
@@ -3,9 +3,19 @@ class SimulationMailer < ApplicationMailer
   def send_it_to_me
     @user = params[:user]
     @simulation = params[:simulation]
-    mail(
-      to: email_address_with_name(@user.email, "#{@user.first_name} #{@user.last_name.upcase}"),
-      subject: "Votre simulation pour le bien de #{@simulation.house_city}"
-    )
+    @unknown_user = params[:unknown_user]
+
+    if @unknown_user.empty?
+      mail(
+        to: email_address_with_name(@user.email, "#{@user.first_name} #{@user.last_name.upcase}"),
+        subject: "Votre simulation pour le bien de #{@simulation.house_city}"
+      )
+    else
+      mail(
+        to: @unknown_user['email'],
+        subject: "Votre simulation pour le bien de #{@simulation.house_city}",
+        template_name: "send_it_to_me_unknown_user"
+      )
+    end
   end
 end
